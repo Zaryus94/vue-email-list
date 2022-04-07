dc8939a2963a0e41db033343fcc928edf6bc8f97
Christian Podda <poddachristian94@gmail.com>
2022-04-07 21:09:33 +0200

    update but not finished

--- js/app.js
+++ js/app.js
@@ -1,2 +1,19 @@
 // console check
 console.log("JS OK!")
+
+new Vue({
+    el "#app",
+    data() {
+        return {
+            emailArray: [],
+            nLoop: 10,
+        }
+    }
+    mounted() {
+        for (let i = 0; i < this.nLoop; i++) {
+            axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then(response => (this.emailArray.push(response.data.response)))
+        }
+    }
+})
